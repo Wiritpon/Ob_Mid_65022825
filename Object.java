952acac3b4048a04f140adb25dd1ae09a3241c60
Wiritpon <ritsawai3@gmail.com>
2023-08-17 11:09:59 +0700

    Create Object.java

---
+++ Object.java
@@ -0,0 +1,53 @@
+public class Object {
+    public static void main(String[] args) {
+        // Object 1p
+        String Product_Name1 = "chair";
+        double Product_Price1 = 500.0;
+
+        System.out.println("Product_Name: " + Product_Name1);
+        System.out.println("Product_Price: " + Product_Price1);
+
+        System.out.println();
+
+        // Object 2
+        String Product_Name2 = "Television";
+        double Product_Price2 = 20000.0;
+        String Brand2 = "Samsung";
+        String Model2 = "Neo QLED";
+
+        System.out.println("Product_Name: " + Product_Name2);
+        System.out.println("Product_Price: " + Product_Price2);
+        System.out.println("Brand: " + Brand2);
+        System.out.println("Model: " + Model2);
+
+         System.out.println();
+
+        // Object 3
+        String Product_Name3 = "Iphone";
+        double Product_Price3 = 40000.0;
+        String Brand3 = "Apple";
+        String Model3 = "Iphone 99";
+        String Operating_System3 = "ios";
+
+        System.out.println("Product_Name: " + Product_Name3);
+        System.out.println("Product_Price: " + Product_Price3);
+        System.out.println("Brand: " + Brand3);
+        System.out.println("Model: " + Model3);
+        System.out.println("Operating_System: " + Operating_System3);
+
+         System.out.println();
+
+         // Object 4
+        String Product_Name4 = "OOP Programming";
+        double Product_Price4 = 250.0;
+        String Author4 = "John Doe";
+        String Number_of_Pages4 = "300";
+
+        System.out.println("Product Name: " + Product_Name4);
+        System.out.println("Product Price: " + Product_Price4);
+        System.out.println("Author: " + Author4);
+        System.out.println("Number_of_Pages: " + Number_of_Pages4);
+
+         System.out.println();
+    }
+}
